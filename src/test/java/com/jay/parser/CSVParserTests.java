59fb524f03d90b998d0fd6dcc30bb32538ca9ff0
Jay <janlely@163.com>
2023-04-29 14:31:32 +0800

    update CSVParserTests

--- src/test/java/com/jay/parser/CSVParserTests.java
+++ src/test/java/com/jay/parser/CSVParserTests.java
@@ -9,12 +9,16 @@ public class CSVParserTests {
 
     @Test
     public void testLine() {
-//        String filed = "\"hello\"\"world\"";
-//        Result result1 = CsvParser.fieldParser().runParser(Buffer.builder().data(filed.getBytes()).build());
-//        assert result1.isSuccess();
-//        String src = "\"hello\"\"world\",\"a.bd,dd\",field3,\"filed4\"\"\"";
-        String src = "\"filed4\"\"addh\"";
-        Result result = CsvParser.lineParser().runParser(Buffer.builder().data(src.getBytes()).build());
-        assert result.isSuccess();
+        String filed = "\"hello\"\"world\"";
+        Result result1 = CsvParser.fieldParser().runParser(Buffer.builder().data(filed.getBytes()).build());
+        assert result1.isSuccess();
+        assert result1.<String>get(0).equals("hello\"world");
+        String src = "\"hello\"\"world\",\"a.bd,dd\",field3,\"field4\"\"\"";
+        Result result2 = CsvParser.lineParser().runParser(Buffer.builder().data(src.getBytes()).build());
+        assert result2.isSuccess();
+        assert result2.<String>get(0).equals("hello\"world");
+        assert result2.<String>get(1).equals("a.bd,dd");
+        assert result2.<String>get(2).equals("field3");
+        assert result2.<String>get(3).equals("field4\"");
     }
 }
